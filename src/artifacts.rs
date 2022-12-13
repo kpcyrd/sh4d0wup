8a6d692f9b81f3330a62f4e11c4481ef7ce272ef
kpcyrd <git@rxv.cc>
2022-12-13 02:50:07 +0100

    Add extract operation for zip

--- src/artifacts.rs
+++ src/artifacts.rs
@@ -11,10 +11,12 @@ use http::header::{HeaderMap, HeaderName, HeaderValue};
 use http::Method;
 use maplit::hashset;
 use md5::Md5;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::io::Cursor;
+use std::io::Read;
 use std::mem;
 use std::path::PathBuf;
 use url::Url;
@@ -29,6 +31,7 @@ pub enum Artifact {
     Infect(InfectArtifact),
     Tamper(TamperArtifact),
     Compress(CompressArtifact),
+    Extract(ExtractArtifact),
     Memory,
 }
 
@@ -79,6 +82,9 @@ impl Artifact {
                 Some(set)
             }
             Artifact::Compress(compress) => Some(hashset![compress.artifact.as_str()]),
+            Artifact::Extract(ExtractArtifact::Zip(extract)) => {
+                Some(hashset![extract.artifact.as_str()])
+            }
             Artifact::Memory => None,
         }
     }
@@ -125,11 +131,20 @@ impl Artifact {
                     .context("Failed to tamper with artifact")?;
                 Ok(Some(buf))
             }
-            Artifact::Compress(artifact) => {
-                info!("Compressing artifact...");
-                let buf = artifact
+            Artifact::Compress(compress) => {
+                info!(
+                    "Compressing artifact {:?} with {:?}...",
+                    compress.artifact, compress.compression
+                );
+                let buf = compress
                     .resolve(&mut plot_extras.artifacts)
-                    .context("Failed to tamper with artifact")?;
+                    .context("Failed to compress artifact")?;
+                Ok(Some(buf))
+            }
+            Artifact::Extract(extract) => {
+                let buf = extract
+                    .resolve(&mut plot_extras.artifacts)
+                    .context("Failed to extract from artifact")?;
                 Ok(Some(buf))
             }
             Artifact::Memory => Ok(None),
@@ -471,6 +486,105 @@ impl CompressArtifact {
             )
         })?;
         compression::compress(self.compression, artifact.as_bytes())
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", content = "extract", rename_all = "kebab-case")]
+pub enum ExtractArtifact {
+    Zip(GenericExtractArtifact),
+}
+
+impl ExtractArtifact {
+    pub fn resolve(&self, artifacts: &mut Artifacts) -> Result<Vec<u8>> {
+        match self {
+            ExtractArtifact::Zip(extract) => {
+                extract.log();
+
+                let artifact = artifacts.get(&extract.artifact).with_context(|| {
+                    anyhow!(
+                        "Referencing artifact that doesn't exist: {:?}",
+                        extract.artifact
+                    )
+                })?;
+
+                let cursor = Cursor::new(artifact.as_ref());
+                let mut archive = zip::ZipArchive::new(cursor).with_context(|| {
+                    anyhow!(
+                        "Failed to open artifact {:?} as zip archive",
+                        extract.artifact
+                    )
+                })?;
+
+                for i in 0..archive.len() {
+                    trace!("Accessing zip entry {}/{}", i, archive.len());
+                    let mut entry = archive
+                        .by_index(i)
+                        .with_context(|| anyhow!("Failed to access entry at index {:?}", i))?;
+
+                    let name = entry.name();
+                    if entry.is_dir() {
+                        trace!("Skipping directory in zip file: {:?}", name);
+                        continue;
+                    }
+
+                    if !extract.matches(name)? {
+                        trace!("Skipping file in zip file: {:?}", name);
+                        continue;
+                    }
+
+                    debug!("Extracting {:?} from archive...", name);
+                    let mut buf = Vec::new();
+                    entry
+                        .read_to_end(&mut buf)
+                        .context("Failed to extract file from zip")?;
+                    return Ok(buf);
+                }
+
+                bail!("Archive did not contain any files");
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GenericExtractArtifact {
+    pub artifact: String,
+    pub path: Option<String>,
+    pub regex: Option<String>,
+}
+
+impl GenericExtractArtifact {
+    pub fn log(&self) {
+        if let Some(path) = &self.path {
+            info!(
+                "Extracting from artifact {:?} with path={:?}...",
+                self.artifact, path
+            );
+        } else if let Some(regex) = &self.regex {
+            info!(
+                "Extracting from artifact {:?} with regex {:?}...",
+                self.artifact, regex
+            );
+        } else {
+            info!(
+                "Extracting first entry from artifact {:?}...",
+                self.artifact
+            );
+        }
+    }
+
+    pub fn matches(&self, name: &str) -> Result<bool> {
+        if let Some(path) = &self.path {
+            Ok(path == name)
+        } else if let Some(regex) = &self.regex {
+            // TODO: doing this in a loop is bad
+            let re = Regex::new(regex)
+                .with_context(|| anyhow!("Failed to compile regex: {:?}", regex))?;
+            Ok(re.is_match(name))
+        } else {
+            Ok(true)
+        }
     }
 }
 

